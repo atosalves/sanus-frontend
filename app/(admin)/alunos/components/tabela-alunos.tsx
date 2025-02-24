4416ac1cd3efeac2cff3b960f92cb4c2f0f6bfa9
Atos Alves <atosalvesqueiroz@gmail.com>
2025-02-24 18:18:48 -0300

    feat: adiciona paginação e atualiza estrutura de alunos

--- app/(admin)/alunos/components/tabela-alunos.tsx
+++ app/(admin)/alunos/components/tabela-alunos.tsx
@@ -1,51 +1,110 @@
 import { Button } from "@/components/ui/button";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { Pencil, Trash2 } from "lucide-react";
+import { AlunoResumidoDTO } from "@/services/alunos-service";
+import { AlertCircle } from "lucide-react";
+import { z } from "zod";
+
+import {
+    Pagination,
+    PaginationContent,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious,
+} from "@/components/ui/pagination";
+
+import { useState } from "react";
+
+const NUM_TOTAL_ALUNOS_POR_PAGINA = 5;
 
 interface TabelaAlunosProps {
-    alunos: {
-        matricula: string;
-        nome: string;
-        email: string;
-        telefone: string;
-        cpf: string;
-        status: string;
-    }[];
+    alunos: z.infer<typeof AlunoResumidoDTO>[];
 }
+
 export default function TabelaAlunos({ alunos }: TabelaAlunosProps) {
+    const numPaginas = Math.ceil(alunos.length / NUM_TOTAL_ALUNOS_POR_PAGINA);
+
+    const [paginaAtual, setPaginaAtual] = useState(1);
+
+    const [alunosPorPagina, setAlunosPorPagina] = useState(alunos.slice(paginaAtual - 1, NUM_TOTAL_ALUNOS_POR_PAGINA));
+
+    function handleProximaPagina() {
+        if (paginaAtual === numPaginas) return;
+
+        setPaginaAtual((prevPag) => prevPag + 1);
+        setAlunosPorPagina(
+            alunos.slice(paginaAtual * NUM_TOTAL_ALUNOS_POR_PAGINA, (paginaAtual + 1) * NUM_TOTAL_ALUNOS_POR_PAGINA)
+        );
+    }
+
+    function handlePaginaAnterior() {
+        if (paginaAtual === 1) return;
+        setPaginaAtual((prevPag) => prevPag - 1);
+        setAlunosPorPagina(
+            alunos.slice(
+                (paginaAtual - 2) * NUM_TOTAL_ALUNOS_POR_PAGINA,
+                (paginaAtual - 1) * NUM_TOTAL_ALUNOS_POR_PAGINA
+            )
+        );
+    }
+
     return (
-        <Table>
-            <TableHeader>
-                <TableRow>
-                    <TableHead className="text-center w-32">Matrícula</TableHead>
-                    <TableHead>Nome</TableHead>
-                    <TableHead>Email</TableHead>
-                    <TableHead className="text-center">Telefone</TableHead>
-                    <TableHead className="text-center">CPF</TableHead>
-                    <TableHead className="text-center">Status do plano</TableHead>
-                    <TableHead className="text-center">Ações</TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                {alunos.map(({ matricula, nome, email, telefone, cpf, status }) => (
-                    <TableRow key={matricula}>
-                        <TableCell className="text-center font-medium">{matricula}</TableCell>
-                        <TableCell>{nome}</TableCell>
-                        <TableCell>{email}</TableCell>
-                        <TableCell className="text-center">{telefone}</TableCell>
-                        <TableCell className="text-center">{cpf}</TableCell>
-                        <TableCell className="text-center">{status}</TableCell>
-                        <TableCell className="text-center space-x-2">
-                            <Button>
-                                <Pencil />
-                            </Button>
-                            <Button variant="destructive">
-                                <Trash2 />
-                            </Button>
-                        </TableCell>
+        <>
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="text-center w-32">Matrícula</TableHead>
+                        <TableHead>Nome</TableHead>
+                        <TableHead className="text-center">Telefone</TableHead>
+                        <TableHead className="text-center">Plano</TableHead>
+                        <TableHead className="text-center">Status do plano</TableHead>
+                        <TableHead className="text-center">Ações</TableHead>
                     </TableRow>
-                ))}
-            </TableBody>
-        </Table>
+                </TableHeader>
+                <TableBody>
+                    {alunos.map(({ matricula, usuario, plano }) => (
+                        <TableRow key={matricula}>
+                            <TableCell className="text-center font-medium">{matricula}</TableCell>
+                            <TableCell>{usuario.nome}</TableCell>
+                            <TableCell className="text-center">{usuario.telefone}</TableCell>
+                            <TableCell className="text-center">{plano.nome}</TableCell>
+                            <TableCell className="text-center">{plano.status}</TableCell>
+                            <TableCell className="text-center space-x-2">
+                                <Button variant="ghost">
+                                    <AlertCircle />
+                                    Mais informações
+                                </Button>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+            <Pagination className="p-2">
+                <PaginationContent>
+                    <PaginationItem className="w-32 justify-items-end">
+                        {paginaAtual > 1 ? (
+                            <PaginationPrevious href={`#${paginaAtual - 1}`} onClick={handlePaginaAnterior} />
+                        ) : (
+                            <PaginationEllipsis />
+                        )}
+                    </PaginationItem>
+
+                    <PaginationItem>
+                        <PaginationLink href={`#${paginaAtual}`} isActive>
+                            {paginaAtual}
+                        </PaginationLink>
+                    </PaginationItem>
+
+                    <PaginationItem className="w-32">
+                        {paginaAtual < numPaginas ? (
+                            <PaginationNext href={`#${paginaAtual + 1}`} onClick={handleProximaPagina} />
+                        ) : (
+                            <PaginationEllipsis />
+                        )}
+                    </PaginationItem>
+                </PaginationContent>
+            </Pagination>
+        </>
     );
 }
