3771a58b5155a7d5515c47f5afc21f111e21b4db
Atos Alves <atosalvesqueiroz@gmail.com>
2025-02-25 18:03:32 -0300

    fix: ajusta layout do painel do admin

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -7,8 +7,10 @@ export default function LayoutAdmin({ children }: { children: ReactNode }) {
     return (
         <SidebarProvider>
             <AppSidebar />
-            <SidebarTrigger className="relative top-5 left-5" />
-            <main className="flex flex-col justify-between w-full h-dvh p-24">{children}</main>
+            <div className="flex flex-col justify-between w-full h-dvh p-6 space-y-4">
+                <SidebarTrigger className="" />
+                {children}
+            </div>
         </SidebarProvider>
     );
 }
