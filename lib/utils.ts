d5bd03c1ddf7fcab862b7105a33a47d732ab9a51
Atos Alves <atosalvesqueiroz@gmail.com>
2025-03-05 18:44:14 -0300

    feat: adiciona função fetcher

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,18 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
+}
+
+export async function fetcher(url: string, options?: RequestInit) {
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+        throw new Error("Algo deu errado: " + response.status);
+    }
+
+    const data = await response.json();
+
+    return data;
 }
