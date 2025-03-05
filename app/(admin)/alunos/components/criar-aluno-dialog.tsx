58ad48d6167abec66190b53627b10abf19dde073
Atos Alves <atosalvesqueiroz@gmail.com>
2025-03-05 19:01:13 -0300

    refactor: atualiza dialog de criar aluno

--- app/(admin)/alunos/components/criar-aluno-dialog.tsx
+++ app/(admin)/alunos/components/criar-aluno-dialog.tsx
@@ -10,15 +10,29 @@ import {
     DialogTrigger,
 } from "@/components/ui/dialog";
 import { Plus } from "lucide-react";
-import FormularioCriarAluno from "./formulario-criar-aluno";
-import { useState } from "react";
-import { useCriarAluno } from "@/hooks/use-aluno";
+
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Switch } from "@/components/ui/switch";
+
+import { InputOTP, InputOTPGroup, InputOTPSlot } from "@/components/ui/input-otp";
+import { REGEXP_ONLY_DIGITS } from "input-otp";
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+
+import { Terminal } from "lucide-react";
+
+import { useCriaAluno } from "@/hooks/aluno/use-cria-aluno";
 
 export default function CriarAlunoDialog() {
-    const [isOpen, setIsOpen] = useState(false);
-    const { mutate, error, isPending } = useCriarAluno();
+    const { isOpen, setIsOpen, form, mutation } = useCriaAluno();
+
+    const { mutate, isPending, error } = mutation;
+
     return (
-        <Dialog onOpenChange={() => setIsOpen((prevUpdate) => !prevUpdate)} open={isOpen}>
+        <Dialog onOpenChange={() => setIsOpen(!isOpen)} open={isOpen}>
             <DialogTrigger asChild>
                 <Button>
                     <Plus />
@@ -33,21 +47,154 @@ export default function CriarAlunoDialog() {
                         "Adiconar".
                     </DialogDescription>
                 </DialogHeader>
-                <FormularioCriarAluno
-                    onSubmit={(modelFormData) =>
-                        mutate(
-                            modelFormData,
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit((alunoFormData) => mutate(alunoFormData))} className="space-y-6">
+                        <FormField
+                            control={form.control}
+                            name="usuario.admin"
+                            render={({ field }) => (
+                                <FormItem className="flex flex-row items-center justify-between">
+                                    <FormLabel>É um usuário administrador?</FormLabel>
+                                    <FormControl>
+                                        <Switch checked={field.value} onCheckedChange={field.onChange} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="usuario.nome"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Nome completo</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="Nome completo" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="usuario.email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input type="email" placeholder="Email" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="usuario.telefone"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Telefone</FormLabel>
+                                    <FormControl>
+                                        <InputOTP maxLength={11} {...field} pattern={REGEXP_ONLY_DIGITS}>
+                                            <span className="font-bold">&#40;</span>
+                                            <InputOTPGroup>
+                                                <InputOTPSlot index={0} />
+                                                <InputOTPSlot index={1} />
+                                            </InputOTPGroup>
+                                            <span className="font-bold">&#41;</span>
+                                            <InputOTPGroup>
+                                                <InputOTPSlot index={2} />
+                                            </InputOTPGroup>
+                                            <InputOTPGroup>
+                                                <InputOTPSlot index={3} />
+                                                <InputOTPSlot index={4} />
+                                                <InputOTPSlot index={5} />
+                                                <InputOTPSlot index={6} />
+                                            </InputOTPGroup>
+                                            <span className="font-bold">-</span>
+                                            <InputOTPGroup>
+                                                <InputOTPSlot index={7} />
+                                                <InputOTPSlot index={8} />
+                                                <InputOTPSlot index={9} />
+                                                <InputOTPSlot index={10} />
+                                            </InputOTPGroup>
+                                        </InputOTP>
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="usuario.cpf"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>CPF</FormLabel>
+                                    <FormControl>
+                                        <InputOTP maxLength={11} {...field} pattern={REGEXP_ONLY_DIGITS}>
+                                            <InputOTPGroup>
+                                                <InputOTPSlot index={0} />
+                                                <InputOTPSlot index={1} />
+                                                <InputOTPSlot index={2} />
+                                            </InputOTPGroup>
+                                            <span className="font-bold">.</span>
+                                            <InputOTPGroup>
+                                                <InputOTPSlot index={3} />
+                                                <InputOTPSlot index={4} />
+                                                <InputOTPSlot index={5} />
+                                            </InputOTPGroup>
+                                            <span className="font-bold">.</span>
+                                            <InputOTPGroup>
+                                                <InputOTPSlot index={6} />
+                                                <InputOTPSlot index={7} />
+                                                <InputOTPSlot index={8} />
+                                            </InputOTPGroup>
 
-                            {
-                                onSuccess: () => {
-                                    setIsOpen((prevUpdate) => !prevUpdate);
-                                },
-                            }
-                        )
-                    }
-                    isPendente={isPending}
-                    erro={error}
-                />
+                                            <span className="font-bold">-</span>
+                                            <InputOTPGroup>
+                                                <InputOTPSlot index={9} />
+                                                <InputOTPSlot index={10} />
+                                            </InputOTPGroup>
+                                        </InputOTP>
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="planoId"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Plano</FormLabel>
+                                    <Select onValueChange={field.onChange}>
+                                        <FormControl>
+                                            <SelectTrigger>
+                                                <SelectValue placeholder="Selecione um plano" {...field} />
+                                            </SelectTrigger>
+                                        </FormControl>
+                                        <SelectContent>
+                                            <SelectItem value="1">BÁSICO</SelectItem>
+                                            <SelectItem value="2">MÉDIO</SelectItem>
+                                            <SelectItem value="3">PREMIUM</SelectItem>
+                                        </SelectContent>
+                                    </Select>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        {error && (
+                            <Alert variant="destructive">
+                                <Terminal className="h-4 w-4" />
+                                <AlertTitle>Houve um problema</AlertTitle>
+                                <AlertDescription>{error.message}</AlertDescription>
+                            </Alert>
+                        )}
+                        <Button type="submit" disabled={isPending}>
+                            Adicionar
+                        </Button>
+                    </form>
+                </Form>
             </DialogContent>
         </Dialog>
     );
