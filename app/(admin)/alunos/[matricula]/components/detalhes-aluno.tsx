77345ca682c8dd053dea1aa8afa5af1c4db23e96
Atos Alves <atosalvesqueiroz@gmail.com>
2025-03-05 19:02:12 -0300

    refactor: melhora a exibição dos detalhes do aluno

--- app/(admin)/alunos/[matricula]/components/detalhes-aluno.tsx
+++ app/(admin)/alunos/[matricula]/components/detalhes-aluno.tsx
@@ -1,11 +1,10 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
-import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
-import { useBuscarAluno } from "@/hooks/use-aluno";
-import AtualizaPlanoAlunoDialog from "../../components/atualizar-plano-aluno-dialog";
+import AtualizaPlanoAlunoDialog from "./atualiza-plano-aluno-dialog";
+import { Carregando } from "@/components/carregando";
+import { useBuscaAluno } from "@/hooks/aluno/use-busca-aluno";
 
 const dataFormatoBR = new Intl.DateTimeFormat("pt-BR", {
     day: "2-digit",
@@ -14,18 +13,16 @@ const dataFormatoBR = new Intl.DateTimeFormat("pt-BR", {
 });
 
 export default function DetalhesAluno({ matricula }: { matricula: string }) {
-    const { data, isLoading, error } = useBuscarAluno(matricula);
+    const { data, isLoading, error } = useBuscaAluno(matricula);
 
-    if (isLoading) {
-        return "Carregando...";
-    }
+    if (isLoading) return <Carregando />;
 
     if (error) {
-        return <div>Error: {error.message}</div>;
+        return <div className="flex justify-center space-x-4 h-full w-full">{error.message}</div>;
     }
 
     if (!data) {
-        return "Não encontrado";
+        return <div className="flex justify-center space-x-4 h-full w-full">Dados não encontrados</div>;
     }
 
     const { usuarioReponseDto, idPlano, planoNome, dataAssinatura, dataVencimento, statusPlano } = data;
@@ -33,72 +30,53 @@ export default function DetalhesAluno({ matricula }: { matricula: string }) {
     const { nome, email, telefone, cpf, admin, ativo } = usuarioReponseDto;
 
     return (
-        <>
-            <Card>
-                <CardHeader>
-                    <CardTitle>Detalhes sobre a conta do aluno:</CardTitle>
-                </CardHeader>
-                <CardContent className="space-y-4">
-                    <div>
-                        <CardDescription>Nome:</CardDescription>
-                        <Label>{nome}</Label>
-                    </div>
-                    <div>
-                        <CardDescription>Email:</CardDescription>
-                        <Label>{email}</Label>
-                    </div>
-                    <div>
-                        <CardDescription>Telefone:</CardDescription>
-                        <Label>{telefone}</Label>
-                    </div>
-                    <div>
-                        <CardDescription>CPF:</CardDescription>
-                        <Label>{cpf}</Label>
-                    </div>
-                    <div>
-                        <CardDescription>Situação da conta:</CardDescription>
-                        <Label>{ativo ? "ATIVADO" : "DESATIVADO"}</Label>
-                    </div>
-                    <div>
-                        <CardDescription>Matrícula:</CardDescription>
-                        <Label>{matricula}</Label>
-                    </div>
-                </CardContent>
-                <CardFooter>
-                    <Label className="text-red-500">
-                        Esta conta {!admin && "não"} possui permissões de administrador
-                    </Label>
-                </CardFooter>
-            </Card>
-            <Card>
-                <CardHeader>
-                    <CardTitle>Detalhes sobre o plano</CardTitle>
-                </CardHeader>
-                <CardContent className="space-y-2">
-                    <div>
-                        <CardDescription>Plano:</CardDescription>
-                        <Label>{planoNome}</Label>
-                    </div>
-                    <div>
-                        <CardDescription>Data de assinatura:</CardDescription>
-                        <Label>{dataFormatoBR.format(new Date(dataAssinatura))}</Label>
-                    </div>
-                    <div>
-                        <CardDescription>Data de vencimento:</CardDescription>
-                        <Label>{dataFormatoBR.format(new Date(dataVencimento))}</Label>
-                    </div>
-                    <div>
-                        <CardDescription>Status do plano:</CardDescription>
-                        <Label>{statusPlano}</Label>
-                    </div>
-                </CardContent>
-                <CardFooter className="space-x-4">
-                    <Link href={`/planos/${idPlano}`}>
-                        <Button>Ver detalhes do plano</Button>
-                    </Link>
-                    <AtualizaPlanoAlunoDialog matricula={matricula} />
-                </CardFooter>
-            </Card>
-        </>
+        <div className="flex flex-col h-full space-y-4">
+            <Label className="text-red-500">Esta conta {!admin && "não"} possui permissões de administrador</Label>
+            <div>
+                <p>Nome:</p>
+                <Label>{nome}</Label>
+            </div>
+            <div>
+                <p>Email:</p>
+                <Label>{email}</Label>
+            </div>
+            <div>
+                <p>Telefone:</p>
+                <Label>{telefone}</Label>
+            </div>
+            <div>
+                <p>CPF:</p>
+                <Label>{cpf}</Label>
+            </div>
+            <div>
+                <p>Situação da conta:</p>
+                <Label>{ativo ? "ATIVADO" : "DESATIVADO"}</Label>
+            </div>
+            <div>
+                <p>Matrícula:</p>
+                <Label>{matricula}</Label>
+            </div>
+
+            <div>
+                <p>Plano:</p>
+                <Label>{planoNome}</Label>
+            </div>
+            <div>
+                <p>Data de assinatura:</p>
+                <Label>{dataFormatoBR.format(new Date(dataAssinatura))}</Label>
+            </div>
+            <div>
+                <p>Data de vencimento:</p>
+                <Label>{dataFormatoBR.format(new Date(dataVencimento))}</Label>
+            </div>
+            <div>
+                <p>Status do plano:</p>
+                <Label>{statusPlano}</Label>
+            </div>
+            <Link href={`/planos/${idPlano}`}>
+                <Button>Ver detalhes do plano</Button>
+            </Link>
+            <AtualizaPlanoAlunoDialog {...{ matricula, idPlano }} />
+        </div>
     );
 }
