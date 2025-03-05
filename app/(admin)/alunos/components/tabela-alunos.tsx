2a2ca15c80440d1bf022b2d971cca065b0a5ec37
Atos Alves <atosalvesqueiroz@gmail.com>
2025-03-05 19:01:51 -0300

    refactor: melhora carregamento e exibição de alunos na tabela

--- app/(admin)/alunos/components/tabela-alunos.tsx
+++ app/(admin)/alunos/components/tabela-alunos.tsx
@@ -5,63 +5,58 @@ import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@
 import { AlertCircle } from "lucide-react";
 
 import Link from "next/link";
-import { useBuscarTodosAlunos } from "@/hooks/use-aluno";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+
+import { Carregando } from "@/components/carregando";
+import { Erro } from "@/components/erro";
+import { CampoVazio } from "@/components/campo-vazio";
+import { useBuscaTodosAlunos } from "@/hooks/aluno/use-busca-todos-alunos";
 
 export default function TabelaAlunos() {
-    const { data, isLoading, isError } = useBuscarTodosAlunos();
+    const { data, isLoading, error } = useBuscaTodosAlunos();
 
-    if (isLoading) return "Carregando";
+    if (isLoading) return <Carregando />;
 
-    if (isError)
+    if (error) return <Erro error={error} />;
+
+    if (!data || data.length === 0)
         return (
-            <Alert variant="destructive">
-                <AlertTitle>Algo deu errado!</AlertTitle>
-                <AlertDescription>Erro ao buscar alunos</AlertDescription>
-            </Alert>
-        );
-
-    if (data?.length === 0)
-        return (
-            <Alert>
-                <AlertTitle>Nenhum aluno encontrado</AlertTitle>
-                <AlertDescription>Clique no botão "Novo aluno" para adicionar um aluno!</AlertDescription>
-            </Alert>
+            <CampoVazio>
+                Nenhum aluno encontrado, clique no botão <span className="font-bold">Novo aluno</span> para adicionar um
+                novo aluno!
+            </CampoVazio>
         );
 
     return (
-        <>
-            <Table>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead className="text-center w-32">Matrícula</TableHead>
-                        <TableHead>Nome</TableHead>
-                        <TableHead className="text-center">Telefone</TableHead>
-                        <TableHead className="text-center">Plano</TableHead>
-                        <TableHead className="text-center">Status do plano</TableHead>
-                        <TableHead className="text-center">Ações</TableHead>
+        <Table>
+            <TableHeader>
+                <TableRow>
+                    <TableHead className="text-center w-32">Matrícula</TableHead>
+                    <TableHead>Nome</TableHead>
+                    <TableHead className="text-center">Telefone</TableHead>
+                    <TableHead className="text-center">Plano</TableHead>
+                    <TableHead className="text-center">Status do plano</TableHead>
+                    <TableHead className="text-center">Ações</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {data.map(({ matricula, usuario, nomePlano, planoStatus }) => (
+                    <TableRow key={matricula}>
+                        <TableCell className="text-center font-medium">{matricula}</TableCell>
+                        <TableCell>{usuario.nome}</TableCell>
+                        <TableCell className="text-center">{usuario.telefone}</TableCell>
+                        <TableCell className="text-center">{nomePlano}</TableCell>
+                        <TableCell className="text-center">{planoStatus}</TableCell>
+                        <TableCell className="text-center space-x-2">
+                            <Link href={`alunos/${matricula}`}>
+                                <Button variant="ghost">
+                                    <AlertCircle />
+                                    Mais informações
+                                </Button>
+                            </Link>
+                        </TableCell>
                     </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {data?.map(({ matricula, usuario, nomePlano, planoStatus }) => (
-                        <TableRow key={matricula}>
-                            <TableCell className="text-center font-medium">{matricula}</TableCell>
-                            <TableCell>{usuario.nome}</TableCell>
-                            <TableCell className="text-center">{usuario.telefone}</TableCell>
-                            <TableCell className="text-center">{nomePlano}</TableCell>
-                            <TableCell className="text-center">{planoStatus}</TableCell>
-                            <TableCell className="text-center space-x-2">
-                                <Link href={`alunos/${matricula}`}>
-                                    <Button variant="ghost">
-                                        <AlertCircle />
-                                        Mais informações
-                                    </Button>
-                                </Link>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </>
+                ))}
+            </TableBody>
+        </Table>
     );
 }
